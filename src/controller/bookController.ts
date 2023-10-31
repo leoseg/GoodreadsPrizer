60c48d265fa2b984a4e187306059bdd88d4ff875
leoseg <leoseeger16@gmail.com>
2023-10-31 15:58:03 -0300

    implemented sse

--- src/controller/bookController.ts
+++ src/controller/bookController.ts
@@ -11,15 +11,24 @@ export class BookController{
     private bookService:BookService = Container.get(BookService)
 
     /**
-     * Update the book prices for a user
+     * Update the book prices for a user with sse
      * @param request request containing the userId in cookkies
      * @param response response containing the status of the request
      */
     public async updateBookPricesForUser(request:Request,response:Response):Promise<void>{
         // const userID = response.locals.user.sub
-        const user = response.locals.user as GoodReadsUser
-        this.bookService.updateBookPricesForUser(user);
-        response.status(200).send("Book price updating in progress");
+        try {
+            response.setHeader('Content-Type', 'text/event-stream');
+            response.setHeader('Cache-Control', 'no-cache');
+            response.setHeader('Connection', 'keep-alive');
+            const user = response.locals.user as GoodReadsUser
+            await this.bookService.updateBookPricesForUser(user);
+            response.write("data: Book price updating finished");
+            response.end()
+        } catch (error) {
+            console.log(error)
+            response.status(500).send("Error updating book prices")
+        }
     }
 
 
