def9a69d452e99cd2bbcb11c32ecc84f90b5370a
leoseg <leoseeger16@gmail.com>
2023-10-31 16:47:23 -0300

    added AWS user deletion

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -2,11 +2,13 @@ import {AppDataSource} from "../db/postgresConfig";
 import {GoodReadsUser} from "../entity/goodReadsUser";
 import {Request,Response} from "express";
 import {Service} from "typedi";
+import AWS from "aws-sdk";
 
 @Service()
 export class UserController {
     private userRepository = AppDataSource.getRepository(GoodReadsUser);
 
+    private cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
     /**
      * Get one user by id
@@ -49,7 +51,26 @@ export class UserController {
         if (!userToRemove) {
             return response.status(404).send("this user not exist");
         }
-        await this.userRepository.remove(userToRemove);
-        return response.status(200).send("user has been removed");
+        try {
+          var params = {
+            AccessToken: request.headers.authorization!.split(" ")[1]
+          };
+          await this.userRepository.remove(userToRemove);
+          await new Promise((resolve, reject) => {
+            this.cognitoidentityserviceprovider.deleteUser(params, async (err, data) => {
+              if (err) {
+                console.log(err, err.stack);
+                await this.userRepository.insert(userToRemove!);
+                reject(err);
+              } else {
+                resolve(data);
+              }
+            });
+          });
+          return response.status(200).send("User deleted successfully");
+        } catch (e) {
+          console.error("Error:", e);
+          return response.status(500).send("Error deleting user");
+        }
     }
 }
